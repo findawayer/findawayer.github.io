f4fff842c9613d16cc5542f5873dcae87b7ff5b2
findawayer <findawayer@gmail.com>
2017-03-10 09:22:57 +0900

    Polyfill requestAnimationFrame + check exception on page load

--- js/animation.js
+++ js/animation.js
@@ -27,18 +27,19 @@
         // store target elements and their settings into `targets` variable
         targets = prepare();
 
-        // disable the whole function on mobile devices and ie8-
-        if ( isException() ) {
-            destroyPreloadLayer(); // destroy preloader
-            return;
-        }
-
         // re-calculate position when user resizes window
         window.addEventListener("resize", prepare);
 
         // on resources loaded,
         window.addEventListener("load", function() {
-            setTimeout(startAnimation, 500); // show throbber explicitely for 0.5 second then start the animation
+
+            // disable the whole function on mobile devices and ie8-
+            if ( isException() ) {
+                destroyPreloadLayer(); // destroy preloader
+            } else {
+                setTimeout(startAnimation, 500); // show throbber explicitely for 0.5 second then start the animation
+            }
+            
         });
 
         // Map target elements and store their settings into an array
@@ -128,5 +129,37 @@
         }
 
     })(document.querySelectorAll("[data-animation]"));
+
+    /**
+     * rAF.js
+     * requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+     * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+     * http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+     */
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                    || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };       
+    }
+
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+    }
+    // end of rAF.js
+
 
 })(window, document);
